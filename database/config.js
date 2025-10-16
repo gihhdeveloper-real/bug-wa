b486702b225d3842c649998e3c9468b85f641cc3
Gihh Developer〽️ <gihhofficial@gmail.com>
2025-10-16 18:20:54 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8387428585:AAEbwdktkRbXwXqPxbnzcZZ2y1SzAx9IjjI",  // Ubah Jadi Token Bot Mu !!!
+  owner: "7955761313", // Ubah Jadi Id Mu !!!
+  port: "3024", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "0.0.0.0" // Ubah Jadi Ip Vps Mu !!!
+};
